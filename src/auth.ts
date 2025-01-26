022d09159e6461a48fc1b7353a8024fa21adb830
Aayush Pagare <aayushpagare21@gmail.com>
2025-01-26 15:00:05 +0530

    feat:add login with google

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,18 @@
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./lib/prisma";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  session: {
+    strategy: "jwt",
+  },
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID! as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET! as string,
+    }),
+  ],
 });
