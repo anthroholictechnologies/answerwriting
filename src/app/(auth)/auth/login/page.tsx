5500ab8f3697b3c956bd4b6f9492f9b548ce6927
Aayush Pagare <aayushpagare21@gmail.com>
2025-01-27 23:58:15 +0530

    ipad issue

--- src/app/(auth)/auth/login/page.tsx
+++ src/app/(auth)/auth/login/page.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 import Link from "next/link";
 export default function LoginPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
+    <div className="grid min-h-svh xl:grid-cols-2">
       <div className="flex flex-col justify-center gap-8 md:p-10">
-        <div className="flex flex-col items-center justify-center lg:items-start">
+        <div className="flex flex-col items-center justify-center xl:items-start">
           <Link href="/">
             <Image
               src="/logos/3_resize.png"
@@ -20,13 +20,13 @@ export default function LoginPage() {
             Craft <ImpactSpan text="Better Answers" /> with AI Precision
           </div>
         </div>
-        <div className="flex lg:flex-1 items-center justify-center">
+        <div className="flex xl:flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
             <LoginForm />
           </div>
         </div>
       </div>
-      <div className="relative hidden bg-muted lg:block">
+      <div className="relative hidden bg-muted xl:block">
         <Image
           src="https://images.unsplash.com/photo-1737535614450-ce142f8e2953?q=80&w=1588&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Image"
