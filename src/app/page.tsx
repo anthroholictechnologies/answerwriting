46684794e2c21a0385b52db5d9014ad35d1b3c6c
Aayush Pagare <aayushpagare21@gmail.com>
2025-01-31 22:27:43 +0530

    add the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Footer from "answerwriting/components/misc/misc_footer";
+import Header from "answerwriting/components/misc/misc_header";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen">
-      <div className="text-[3rem] text-center h-[800px]"> Check the footer in phone and laptop </div>
+    <div className="h-screen w-full">
+      <Header />
+      <div className="text-[1rem] text-center h-[800px]"> Check the footer in phone and laptop </div>
       <Footer />
     </div>
   );
---
+++ src/components/misc/misc_header.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { Gift } from "lucide-react";
+const Header = () => {
+  return (
+    <header>
+      <nav className="flex w-full justify-around py-8 xl:px-48">
+        <div className="flex-1 hidden xl:block hover:cursor-pointer">
+          <Image
+            src="/logos/3_resize.png"
+            alt="answerwriting logo"
+            width={200}
+            height={40}
+            className="hidden xl:block h-[40px] w-[200px] ml-4"
+          />
+        </div>
+        <div className="items-center justify-end gap-4 flex-1 hidden xl:flex">
+          <div className="hidden hover:cursor-pointer h-8 w-8 xl:flex justify-center items-center rounded-full shadow border border-[lightGray]">
+            <Gift className="text-primary-dark" />
+          </div>
+          <Button className="py-5 hover:cursor-pointer"> Learn More </Button>
+          <Button variant="transparent" className="py-5 hover:cursor-pointer">
+            {" "}
+            View Timetable{" "}
+          </Button>
+        </div>
+
+        {/* for phone */}
+        <div className="xl:hidden hover:cursor-pointer">
+          <Image
+            src="/logos/1.png"
+            alt="answerwriting logo"
+            width={60}
+            height={60}
+            className="xl:hidden h-[60px] w-[60px] ml-4"
+          />
+        </div>
+        <div className="justify-end gap-1 flex xl:hidden items-center">
+          <Button size="sm" className="hover:cursor-pointer">
+            {" "}
+            Learn More{" "}
+          </Button>
+          <Button
+            size="sm"
+            variant="transparent"
+            className="hover:cursor-pointer"
+          >
+            {" "}
+            View Timetable{" "}
+          </Button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
