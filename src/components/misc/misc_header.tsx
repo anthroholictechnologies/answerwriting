97243b0c2906d16d10f813cb9268044f412ed494
Aayush Pagare <aayushpagare21@gmail.com>
2025-02-01 12:57:50 +0530

    add the robots.txt

--- src/components/misc/misc_header.tsx
+++ src/components/misc/misc_header.tsx
@@ -1,19 +1,23 @@
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { Gift } from "lucide-react";
+import Link from "next/link";
+import { ApiRoutePaths } from "answerwriting/types/general.types";
 const Header = () => {
   return (
     <header>
       <nav className="flex w-full justify-around py-8 xl:px-24">
-        <div className="flex-1 hidden xl:block hover:cursor-pointer">
-          <Image
-            src="/logos/3_resize.png"
-            alt="answerwriting logo"
-            width={200}
-            height={40}
-            className="hidden xl:block h-[40px] w-[200px]"
-          />
-        </div>
+        <Link href={ApiRoutePaths.PAGE_HOME}>
+          <div className="flex-1 hidden xl:block hover:cursor-pointer">
+            <Image
+              src="/logos/3_resize.png"
+              alt="answerwriting logo"
+              width={200}
+              height={40}
+              className="hidden xl:block h-[40px] w-[200px]"
+            />
+          </div>
+        </Link>
         <div className="items-center justify-end gap-4 flex-1 hidden xl:flex">
           <div className="hidden hover:cursor-pointer h-8 w-8 xl:flex justify-center items-center rounded-full shadow border border-[lightGray]">
             <Gift className="text-primary-dark" />
