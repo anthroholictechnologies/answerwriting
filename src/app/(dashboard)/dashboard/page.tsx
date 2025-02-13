0f2f6d544973e5e407ca743b2914774ea45426f6
Aayush Pagare <aayushpagare21@gmail.com>
2025-02-13 21:45:43 +0530

    dummy deploy

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -2,5 +2,5 @@ import { ApiRoutePaths } from "answerwriting/types/general.types";
 import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
-  redirect(ApiRoutePaths.PAGE_DASHBOARD_USER_PROFILE);
+  redirect(ApiRoutePaths.PAGE_DASHBOARD_USER_PROFILE)
 }
