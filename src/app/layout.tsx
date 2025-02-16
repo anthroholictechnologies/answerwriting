707548cb27c67367ed495d59f37c942f0cf29c30
Aayush Pagare <aayushpagare21@gmail.com>
2025-02-16 18:56:13 +0530

    Add google tag manager's code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "answerwriting/components/ui/toaster";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const openSans = Open_Sans({
   variable: "--font-open-sans",
@@ -20,7 +21,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-M3XH7KGZ" />
       <body className={`${openSans.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M3XH7KGZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {children}
         <Toaster />
       </body>
